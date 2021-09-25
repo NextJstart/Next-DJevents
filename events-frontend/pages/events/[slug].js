8eddf943fa4e52999eeb1f8c3e6166a56cf32a9e
TanyaRan <tatyanaran@gmail.com>
2021-09-25 22:01:08 +0300

    Link and router, added Link and redirect

--- events-frontend/pages/events/[slug].js
+++ events-frontend/pages/events/[slug].js
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/router';
+
 export default function Event() {
+	const router = useRouter();
+
+	console.log(router);
+
 	return (
 		<div>
 			<h1>Single Event</h1>
+			<h3>{router.query.slug}</h3>
+			<button onClick={() => router.push('/')}>Click to redirect</button>
 		</div>
 	);
 }
