f3a89fcd13259ba175cf9c1ac2c2e37395407b0b
TanyaRan <tatyanaran@gmail.com>
2021-09-26 02:03:45 +0300

    created Layout component, insert Head, insert globals and Layout styles

--- events-frontend/pages/events/[slug].js
+++ events-frontend/pages/events/[slug].js
@@ -1,15 +1,9 @@
-import { useRouter } from 'next/router';
+import Layout from '../../components/Layout';
 
 export default function Event() {
-	const router = useRouter();
-
-	console.log(router);
-
 	return (
-		<div>
+		<Layout>
 			<h1>Single Event</h1>
-			<h3>{router.query.slug}</h3>
-			<button onClick={() => router.push('/')}>Click to redirect</button>
-		</div>
+		</Layout>
 	);
 }
